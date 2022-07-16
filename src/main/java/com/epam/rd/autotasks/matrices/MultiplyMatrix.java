d10e7fe18b12670ad064c8f10703fda9bf823fec
Denys Kovika <d.v.kovika@gmail.com>
2022-07-16 09:54:02 +0300

    ver.1

--- src/main/java/com/epam/rd/autotasks/matrices/MultiplyMatrix.java
+++ src/main/java/com/epam/rd/autotasks/matrices/MultiplyMatrix.java
@@ -4,9 +4,32 @@ import java.util.Arrays;
 public class MultiplyMatrix {
     public static int[][] multiply(int[][] matrix1, int[][] matrix2) {
 
-        // Put your code here
+        int row1 = matrix1.length;
+        int col1 = matrix1[0].length;
+        int row2 = matrix2.length;
+        int col2 = matrix2[0].length;
+        int i, j, k;
 
-        return null;
+
+            // Check if multiplication is Possible
+            if (row2 != col1) {
+
+                System.out.println(
+                        "\nMultiplication Not Possible");
+                return null;
+            }
+
+            int resArr[][] = new int[row1][col2];
+
+
+            for (i = 0; i < row1; i++) {
+                for (j = 0; j < col2; j++) {
+                    for (k = 0; k < row2; k++)
+                        resArr[i][j] += matrix1[i][k] * matrix2[k][j];
+                }
+            }
+
+        return resArr;
     }
 
     public static void main(String[] args) {
